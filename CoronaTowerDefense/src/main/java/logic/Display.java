e8c3fae1450e5f290a2cfb6486cce67ef7eb1b67
Valokoodari <28293838+Valokoodari@users.noreply.github.com>
2020-04-28 11:52:46 +0300

    Javadoc näyttötiedoille

--- CoronaTowerDefense/src/main/java/logic/Display.java
+++ CoronaTowerDefense/src/main/java/logic/Display.java
@@ -4,6 +4,12 @@ public class Display {
 	public int width;
 	public int height;
 	
+	/**
+	 * Luo olion säilyttämään tietoja nykyisestä näytöstä
+	 * 
+	 * @param width  Näytön leveys pikseleissä
+	 * @param height  Näytön korkeus pikseleissä
+	 */
 	public Display(int width, int height) {
 		this.width = width;
 		this.height = height;
